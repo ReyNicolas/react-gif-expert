3341732dbf92408bfc383e985bd84ae6da3cea2a
Nicolas <rey.nicolas.92@gmail.com>
2022-11-23 21:54:38 -0300

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+
+
+export const AddCategory = ({ onNewCategory }) => {
+  
+    const [inputValue, setInputValue] = useState('')
+  
+    const onInputValue = ({ target }) => {
+
+        setInputValue(target.value);
+    }
+
+
+    const onSubmit =  ( event ) => {
+
+        event.preventDefault();
+        console.log( inputValue );
+
+        if (inputValue.trim().length <=1) return;
+
+        onNewCategory( inputValue.trim() );
+        //setCategories( (categories) => [ inputValue, ...categories ]);
+        setInputValue('');
+    }
+  
+    return (
+
+        <form onSubmit={ onSubmit }>
+            <input 
+                type="text"
+                placeholder="Buscar gifs"
+                value = { inputValue }
+                onChange=  { onInputValue }
+            />
+        </form>
+    
+  )
+}
